c2813a5b296161462923dcdcf937922ca0bff567
Shivani <165663972+shivanialley@users.noreply.github.com>
2024-04-11 11:46:22 +0530

    Add files via upload

---
+++ dayOne/fun.cpp
@@ -0,0 +1,19 @@
+// function code
+// declaration
+#include<iostream>
+
+using namespace std;
+
+void fun();
+//main part where compiler now
+int main()
+{
+	cout<<"Printing..."<<endl;
+	fun();
+}
+//defination
+void fun()
+{
+	cout<<"Inside the fun()"<<endl;
+}
+
