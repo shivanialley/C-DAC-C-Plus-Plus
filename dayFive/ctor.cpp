44ea65fd32e83da381367614f509407789cea2a6
Shivani <165663972+shivanialley@users.noreply.github.com>
2024-04-11 19:50:35 +0530

    Add files via upload

---
+++ dayFive/ctor.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+public:
+	Base(){ cout<<"Base::Base()"<<endl; }
+	~Base(){ cout<<"Base::~Base()"<<endl; }
+};
+
+int main(){
+    Base b;
+
+}
